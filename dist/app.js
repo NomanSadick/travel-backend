f21d602d9e4d5fee44b3b56eea0b5ca07ecee868
Noman Sadick <nomansadickjs@gmail.com>
2025-05-05 10:42:21 +0600

    Refactor CORS configuration to remove redundant middleware and ensure proper usage

--- dist/app.js
+++ dist/app.js
@@ -7,9 +7,13 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const packageRoutes_1 = __importDefault(require("./routes/packageRoutes"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    origin: "https://travel-frontend-6rc3.vercel.app/",
+    credentials: true
+}));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use((0, cors_1.default)());
+// app.use(cors());
 app.use(express_1.default.json());
 app.use('/api/packages', packageRoutes_1.default);
 app.get('/', (_req, res) => {
--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,12 @@ import packageRoutes from './routes/packageRoutes';
 const app = express();
 
 app.use(cors({
-  origin: "https://travel-frontend-6rc3.vercel.app/", // <-- এখানে তোমার frontend domain বসাও
+  origin: "https://travel-frontend-6rc3.vercel.app/",
   credentials: true
 }));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use('/api/packages', packageRoutes);
